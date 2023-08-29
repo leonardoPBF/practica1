0ed3296c210bc9a81acc2a22ab73c8ea03d5e6e4
leonardoPBF <leonardobf140224@gmail.com>
2023-08-28 21:49:14 -0500

    update 2

--- pages/contact/proses.php
+++ pages/contact/proses.php
@@ -24,43 +24,43 @@ if (isset($_POST['send'])) {
     if ($query) {
         $mail = new PHPMailer(true);
 
-    try {
-        //Server settings
-        $mail->SMTPDebug = 0;                      //Enable verbose debug output
-        $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.office365.com';                     //Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'leonardo14bf@hotmail.com';                     //SMTP username
-        $mail->Password   = 'pokemon14';                               //SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
-        $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        try {
+            //Server settings
+            $mail->SMTPDebug = 0;                      //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'smtp.office365.com';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = 'leonardo14bf@hotmail.com';                     //SMTP username
+            $mail->Password   = 'pokemon14';                               //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+            $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-        //Recipients
-        $mail->setFrom('leonardo14bf@hotmail.com', 'Soluciones en sistemas');
-        $mail->addAddress("$email", 'Cliente');     //Add a recipient
+            //Recipients
+            $mail->setFrom('leonardo14bf@hotmail.com', 'Soluciones en sistemas');
+            $mail->addAddress("$email", 'Cliente');     //Add a recipient
 
-        //$mail->addCC('cc@example.com');
-        //$mail->addBCC('bcc@example.com');
+            //$mail->addCC('cc@example.com');
+            //$mail->addBCC('bcc@example.com');
 
-        //Attachments
-        //$mail->addAttachment('/v    ar/tmp/file.tar.gz');         //Add attachments
-        //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+            //Attachments
+            //$mail->addAttachment('/v    ar/tmp/file.tar.gz');         //Add attachments
+            //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
-        //Content
-        $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = 'Gracias por contactarnos';
-        $mail->Body    = 'Recibimos tu mensaje satifactoriamente';
-        $mail->AltBody = 'Tomaremos en cuenta tu mensaje, hablanos pronto";
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = 'Gracias por contactarnos';
+            $mail->Body    = 'Recibimos tu mensaje satifactoriamente';
+            $mail->AltBody = 'Tomaremos en cuenta tu mensaje, hablanos pronto';
 
-        $mail->send();
-        echo 'Message has been sent';
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    }
+            $mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
         
         header("location: ../../contact-success");
     }  
 
-    
+
 }   
 ?>
